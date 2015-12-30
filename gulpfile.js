948604b175a2d69e31e16facd4fa7e7a4ac4858a
Tao <Tao@macs-Mac-mini.local>
2015-12-30 18:04:09 +0800

    browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@
 
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    open = require('gulp-open');
+    open = require('gulp-open'),
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('vinyl-source-stream');
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './build/**/*.html',
+        module: './build/module.js',
         dist: './public'
     }
 };
@@ -28,13 +32,24 @@ gulp.task('html', function(){
         .pipe(connect.reload());
 });
 
+gulp.task('module', function() {
+    browserify(config.paths.module)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('module.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
 gulp.task('open', ['connect'], function(){
    gulp.src('public/index.html')
-       .pipe(open({url: config.devBaseUrl + ':' + config.port + '/'}));
+       .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
 });
 
 gulp.task('watch', function(){
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.module, ['module']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'module', 'open', 'watch']);
