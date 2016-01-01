1fcf6c51e1eecd33725f475c3ae3e5e1204af4c5
Tao <Tao@macs-Mac-mini.local>
2016-01-01 08:05:49 +0800

    vanilla routing

--- build/component.js
+++ build/component.js
@@ -3,5 +3,27 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var ReactDom = require('react-dom');
 var Index = require('./components/index');
+var About = require('./components/about');
 
-ReactDom.render(<Index />, document.getElementById('main'));
+var App = React.createClass({
+    render: function() {
+        var Child;
+
+        switch (this.props.route) {
+            case 'about': Child = About; break;
+            default: Child = Index;
+        }
+
+        return (
+            <Child />
+        );
+    }
+});
+
+function render() {
+    var route = window.location.hash.substr(1);
+    ReactDom.render(<App route={route} />, document.getElementById('main'));
+}
+
+window.addEventListener('hashchange', render);
+render();
---
+++ build/components/about.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+var About = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Agile About</h1>
+                <p>About React agile web application</p>
+            </div>
+        );
+    }
+});
+
+module.exports = About;
